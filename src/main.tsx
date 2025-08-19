bf0fdd3452fad0d1a392bc5e69dbc0f59bf3f522
parksangseok <park0157@naver.com>
2025-08-19 10:21:47 +0900

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,12 @@ import './styles/fonts.css';
 import './styles/global.css';
 
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>
 )
