446331f5049f4e7e47534eeda577049e966afc45
parksangseok <park0157@naver.com>
2025-08-20 17:42:43 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,27 @@ import { Routes, Route } from 'react-router-dom';
 
 import Compoents from './pages/test/Compoents';
 import Home from './pages/Home';
+import { useEffect, useState } from 'react';
+import FullScreenLoading from './components/ui/FullScreenLoading';
 
 
 function App() {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    setTimeout(() => setLoading(false), 1500);
+
+  }, [])
+
+  if (loading) {
+    return (
+      <>
+        <FullScreenLoading color="#0F7685" />
+      </>
+    )
+  }
 
   return (
     <Routes>
