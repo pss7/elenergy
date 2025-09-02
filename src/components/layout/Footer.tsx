be4f169ea74c32db0e6f353a66657de103398171
parksangseok <park0157@naver.com>
2025-09-02 14:34:54 +0900

    commit

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,52 +1,64 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
-
   const isLoggedIn = localStorage.getItem("isLoggedIn") === "true";
 
   return (
-    <>
-      <footer id={styles.footer}>
-        <ul className={styles.link}>
-          <li>
-            <Link to="/scheduled-block">
-              <span>
-                예약차단
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/auto-block">
-              <span>
-                자동차단
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/">
-              <span>
-                메인
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/monitoring">
-              <span>
-                모니터링
-              </span>
-            </Link>
-          </li>
-          <li>
-            <Link to={isLoggedIn ? "/my" : "/signin"}>
-              <span>
-                MY
-              </span>
-            </Link>
-          </li>
-        </ul>
-      </footer>
-    </>
-  )
-
+    <footer id={styles.footer}>
+      <ul className={styles.link}>
+        <li>
+          <NavLink
+            to="/scheduled-block"
+            className={function ({ isActive }) {
+              return isActive ? styles.active : "";
+            }}
+          >
+            <span>예약차단</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/auto-block"
+            className={function ({ isActive }) {
+              return isActive ? styles.active : "";
+            }}
+          >
+            <span>자동차단</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/"
+            end
+            className={function ({ isActive }) {
+              return isActive ? styles.active : "";
+            }}
+          >
+            <span>메인</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/monitoring"
+            className={function ({ isActive }) {
+              return isActive ? styles.active : "";
+            }}
+          >
+            <span>모니터링</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={isLoggedIn ? "/my" : "/signin"}
+            className={function ({ isActive }) {
+              return isActive ? styles.active : "";
+            }}
+          >
+            <span>프로필</span>
+          </NavLink>
+        </li>
+      </ul>
+    </footer>
+  );
 }
